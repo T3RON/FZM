1234fb9f012b51555349ed449b3e7990d262f275
MohammadSeif <seif4847@gmail.com>
2019-02-26 10:02:07 +0330

    Create Activity
    Login ,  Sms

---
+++ app/src/main/java/ir/mseif/app/com/fzm/Panels/Sms.java
@@ -0,0 +1,15 @@
+package ir.mseif.app.com.fzm.Panels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.mseif.app.com.fzm.R;
+
+public class Sms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+    }
+}
