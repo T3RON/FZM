f42b31fa2c81b35d3e532e1fdb8f93af5b0eadd4
Luna <luna@MacBook-Pro.local>
2019-03-03 10:10:38 +0330

    activity_login_Create
    activity loging V1.0

--- app/src/main/java/ir/mseif/app/com/fzm/Panels/Login.java
+++ app/src/main/java/ir/mseif/app/com/fzm/Panels/Login.java
@@ -1,9 +1,11 @@
 package ir.mseif.app.com.fzm.Panels;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ir.mseif.app.com.fzm.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Login extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
