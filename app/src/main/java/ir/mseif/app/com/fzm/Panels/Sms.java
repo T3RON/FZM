d6570768d4aa818333183442f357523963ad3c60
Luna <luna@MacBook-Pro.local>
2019-03-03 10:38:20 +0330

    activity_login_create
    activity_login V 1.0

--- app/src/main/java/ir/mseif/app/com/fzm/Panels/Sms.java
+++ app/src/main/java/ir/mseif/app/com/fzm/Panels/Sms.java
@@ -1,9 +1,11 @@
 package ir.mseif.app.com.fzm.Panels;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ir.mseif.app.com.fzm.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Sms extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class Sms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+    }
+
+
+
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
