50cf434b1cb70002d4b7a13d441f38bd7d69ebb2
Luna <luna@MacBook-Pro.local>
2019-03-06 11:29:30 +0330

    page camera create
    Page Camera V 1.1
    Page Asansor V 1.3
    Page Main V 1.4

--- app/src/main/java/ir/mseif/app/com/fzm/Asansor.java
+++ app/src/main/java/ir/mseif/app/com/fzm/Asansor.java
@@ -1,6 +1,7 @@
 package ir.mseif.app.com.fzm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -65,6 +66,13 @@ public class Asansor extends AppCompatActivity {
 //        }
 //        return String.valueOf(b);
 
+    }
+
+
+    // Go To Time Activity
+    public void Go_To_Time (View v){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 
 
