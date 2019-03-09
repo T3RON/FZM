bfd69113ea34d7793ec37647fe308faba01c5952
Luna <luna@MacBook-Pro.local>
2019-03-09 09:47:45 +0330

    Update Part 2
    page Antenna create V 1.0
    page E_panel create V 1.0
    page Iphone create V 1.0
    page wiring create V 1.0
    page sentral create V 1.0
    page camera update V 1.5

--- app/src/main/java/ir/mseif/app/com/fzm/MainActivity.java
+++ app/src/main/java/ir/mseif/app/com/fzm/MainActivity.java
@@ -21,13 +21,45 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    // Go to Camera Page
+    public void page_camera (View v){
+        Intent intent = new Intent(getApplicationContext(), Camera.class);
+        startActivity(intent);
+    }
 
-
-
+    // Go to Iphon Page
+    public void page_iphon (View v){
+        Intent intent = new Intent(getApplicationContext(), Iphon.class);
+        startActivity(intent);
+    }
 
     // Go to Asansor Page
     public void page_asansor (View v){
         Intent intent = new Intent(getApplicationContext(), Asansor.class);
+        startActivity(intent);
+    }
+
+    // Go to Santeral Page
+    public void page_santeral (View v){
+        Intent intent = new Intent(getApplicationContext(), Santeral.class);
+        startActivity(intent);
+    }
+
+    // Go to Wiring Page
+    public void page_wiring (View v){
+        Intent intent = new Intent(getApplicationContext(), Wiring.class);
+        startActivity(intent);
+    }
+
+    // Go to Antenna Page
+    public void page_e_panel (View v){
+        Intent intent = new Intent(getApplicationContext(), E_panel.class);
+        startActivity(intent);
+    }
+
+    // Go to Antenna Page
+    public void page_antenna (View v){
+        Intent intent = new Intent(getApplicationContext(), Antenna.class);
         startActivity(intent);
     }
 
